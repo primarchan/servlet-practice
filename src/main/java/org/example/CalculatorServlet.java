f81e675591a9f31da61bad6afce3bbf24f780172
An T.G <boltcarrier@naver.com>
2022-12-24 01:56:38 +0900

    first commit

---
+++ src/main/java/org/example/CalculatorServlet.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CalculatorServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+}
